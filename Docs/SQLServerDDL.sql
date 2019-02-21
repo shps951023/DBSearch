8bb9be21aa5bf7fd5943bc32ddc6188e5f0b2925
ITWeiHan <shps951002@gmail.com>
2019-02-21 11:44:17 +0800

    sql server test ddl

--- Docs/SQLServerDDL.sql
+++ Docs/SQLServerDDL.sql
@@ -1,3 +1,33 @@
+﻿
+CREATE TABLE [Table1](
+    [col_varchar] [varchar](150) NULL,
+    [col_varchar_like] [varchar](150) NULL,
+    [col_nvarchar] [nvarchar](300) NULL,
+    [col_int] [int] NULL,
+    [col_datetime] [datetime] NULL,
+    [col_bit] [bit] null,
+    [col_null] [varchar],
+    [col_float] [float]
+);
 
+CREATE TABLE [Table2](
+    [col_varchar] [varchar](150) NULL,
+    [col_varchar_like] [varchar](150) NULL,
+    [col_nvarchar] [nvarchar](300) NULL,
+    [col_int] [int] NULL,
+    [col_datetime] [datetime] NULL,
+    [col_bit] [bit] null,
+    [col_null] [varchar],
+    [col_float] [float]
+);
 
+insert into [Table1] 
+select 'Test','Test2',N'測試',123,'2019/01/02 03:04:05',1
+;
 
+insert into [Table2] 
+select 'Test','Test2',N'測試',123,'2019/01/02 03:04:05',1
+;
+ 
+select * from [Table1];
+select * from [Table2];
